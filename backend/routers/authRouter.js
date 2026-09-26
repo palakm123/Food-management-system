380639725dd7bc52653470693ae1d05b581b418e
Aashi17v <aashiv.work@gmail.com>
2026-09-26 15:27:39 +0530

    added signup

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,5 +1,38 @@
-const express= require("express");
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const protect = require("../middleware/authMiddleware");
+const router = express.Router();
 
-const authRouter= express.router();
+router.post("/signup", protect, async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({
+      uid: req.user.uid
+    });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      uid: req.user.uid,
+      name,
+      email,
+      password: hashedPassword
+    });
 
-authRouter.post("/login",login)
+    res.status(201).json({
+      message: "User created successfully",
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+module.exports = router;
